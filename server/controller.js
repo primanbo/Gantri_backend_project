7689a0e7692955cabfd7363175db110404abfa1b
Priyanka Piali Pramanik <priyanka.pramanik@gmail.com>
2019-12-04 20:15:12 -0800

    fix: Correct addComments controller

--- server/controller.js
+++ server/controller.js
@@ -3,15 +3,15 @@ const { pool } = require('./config');
 const getComments = art => {
   return pool
     .query('SELECT * FROM comments WHERE artid=$1', [art.id])
+    .then( results => {
+      art.comments = results.rows;
+      return art;
+    })
     .catch( err => 
       res
         .status(500)
         .send({ status: `something went wrong`, message: `art could not be retrieved. ${err.message}` })
-    )
-    .then( results => {
-      art.comments = results.rows;
-      return art;
-    });
+    );
 };
 
 const anAsyncFunction = async art => {
@@ -27,20 +27,20 @@ module.exports = {
 
     pool
       .query('SELECT * FROM art')
-      .catch( err => 
-        res
-          .status(500)
-          .send({ status: `something went wrong`, message: `art could not be retrieved. ${err.message}` })
-      )
       .then( results => {
-        
         getArtWithComments(results.rows)
           .then(allArt => {
             res
               .status(200)
               .json(allArt);
           });
-      });
+      })
+      .catch( err => 
+        res
+          .status(500)
+          .send({ status: `something went wrong`, message: `art could not be retrieved. ${err.message}` })
+      );
+
   },
   
   getOneArt: (req, res) => {
@@ -48,11 +48,6 @@ module.exports = {
   
     pool
       .query('SELECT * FROM art WHERE id=$1', [ID])
-      .catch( err =>
-        res
-          .status(500)
-          .send({ status: `something went wrong`, message: `art ${ID} could not be retrieved. ${err.message}` })
-      )
       .then( results => {
         getArtWithComments(results.rows)
           .then(art => {
@@ -60,59 +55,88 @@ module.exports = {
               .status(200)
               .json(art[0]);
           });
-      });
-  },
-  
-  addComment: (req, res) => {
-    const { name, content, userID } = req.body;
-    const userid_fix = Number(userID)===0? NULL:Number(userID);
-    const { ID } = req.params;
-  
-    let queryStr = 'INSERT INTO comments (artid, name, content, userid) SELECT $1, $2, $3, $4 WHERE NOT EXISTS (SELECT 1 FROM comments WHERE userid is NULL AND name = $2)';
-  
-    pool
-      .query(queryStr, [ID, name, content, userid_fix])
+      })
       .catch( err =>
         res
           .status(500)
-          .send({ status: `something went wrong`, message: `comment could not be added. ${err.message}` })
-      )
-      .then( () =>
-        res
-          .status(201)
-          .send({ status: 'success', message: 'comment added.' })
+          .send({ status: `something went wrong`, message: `art ${ID} could not be retrieved. ${err.message}` })
       );
+
+  },
+  
+  addComment: (req, res) => {
+
+    const { name, content, userID } = req.body;
+    const { ID } = req.params;
+
+    let queryStr;
+    let queryParams;
+
+    if (userID === undefined && name === undefined) {
+      res
+        .status(500)
+        .send({ status: `something went wrong`, message: `comment could not be added. either name or userID is required.` });
+      return;
+
+    } 
+    if (typeof userID !== `undefined`) {
+      queryStr = 'INSERT INTO comments (artid, content, userid, name) SELECT $1, $2, $3, name FROM users WHERE userid = $3';
+      queryParams = [ID, content, userID];
+      
+    } else {
+      queryStr = `INSERT INTO comments (artid, content, userid, name) SELECT $1, $2, NULL, $3 WHERE NOT EXISTS (SELECT 1 FROM comments WHERE userid is NULL AND name='$3')`;
+      queryParams = [ID, content, name];
+      
+    }
+    
+    pool
+      .query(queryStr, queryParams)
+      .then( () => {
+        res
+        .status(201)
+        .send({ status: 'success', message: 'comment added.' });
+      })
+      .catch( err => {
+          res
+            .status(500)
+            .send({ status: `something went wrong`, message: `comment could not be added. ${err.message}` });
+      });
+
   },
   
   addUser: (req, res) => {
+
     const { name, age, location } = req.body;
   
     pool
       .query('INSERT INTO users (name, age, location) VALUES ($1, $2, $3)', [name, age, location])
-      .catch( err => 
-        res
-          .status(500)
-          .json({ status: `something went wrong`, message: `user ${name} could not be added. ${err.message}` })
-      )
       .then( () =>
         res
           .status(201)
           .json({ status: `success`, message: `user ${name} added.` })
+      )
+      .catch( err => 
+        res
+          .status(500)
+          .json({ status: `something went wrong`, message: `user ${name} could not be added. ${err.message}` })
       );
+
   },
   
   getUsers: (req, res) => {
+
     pool
       .query('SELECT * FROM users')
-      .catch( err =>
-        res
-        .status(500)
-        .json({ status: `something went wrong`, message: `users could not be retrieved. ${err.message}` })
-      )
       .then( results => 
         res
         .status(200)
         .json(results.rows)
+      )
+      .catch( err =>
+        res
+        .status(500)
+        .json({ status: `something went wrong`, message: `users could not be retrieved. ${err.message}` })
       );
+
   },
 };
