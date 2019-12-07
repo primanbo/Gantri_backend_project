d017ebfd7fa00969395ee7491b2bd9f7e014093a
Priyanka Piali Pramanik <priyanka.pramanik@gmail.com>
2019-12-07 01:28:51 -0800

    fix: Update schema to handle long text and null

--- schema.sql
+++ schema.sql
@@ -1,23 +1,23 @@
 CREATE TABLE users (
   id SERIAL PRIMARY KEY,
-  name VARCHAR(50) NOT NULL, 
+  name TEXT NOT NULL, 
   age INTEGER NOT NULL, 
-  location VARCHAR(50) NOT NULL
+  location TEXT NOT NULL
 );
 
 CREATE TABLE art (
   id SERIAL PRIMARY KEY,
-  title VARCHAR(50) NOT NULL, 
-  artist VARCHAR(50) NOT NULL,
-  year INTEGER NOT NULL 
+  title TEXT NOT NULL, 
+  artist TEXT NOT NULL,
+  year INTEGER 
 );
 
 CREATE TABLE comments (
   id SERIAL PRIMARY KEY,
   artid INTEGER REFERENCES art(id) NOT NULL,
-  userid INTEGER REFERENCES users(userid),
-  name VARCHAR(50) NOT NULL, 
-  content VARCHAR(50) NOT NULL 
+  userid INTEGER REFERENCES users(id),
+  name TEXT,
+  content TEXT NOT NULL 
 );
 
 CREATE UNIQUE INDEX unique_name_when_userid_null
